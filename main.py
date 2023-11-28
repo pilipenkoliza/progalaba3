5839e987c75c91c09bc51a5b41dce5c554a42e76
pilipenkoliza <pilipa2004@gmail.com>
2023-11-28 22:19:05 +0300

    changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import re
-
-def extract_emails(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-        emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', content)
-        return emails
-
-file_path = '/home/ivan/PycharmProjects/4 lab for stankin/file.html'
-emails = extract_emails(file_path)
-print("Extracted Emails:", emails)
+def email(html_file):
+    with open(html_file, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+        emails_html = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', html_content)
+        return emails_html
+html_file = "C:/Users/pilip/PycharmProjects/pythonProject3/xtmls.html"
+emails = email(html_file)
+print(emails)
